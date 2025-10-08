c33e310137ef08d59437963faa3537d5dc28d4ea
VictorBecerra <152622280+BecerraSoftware@users.noreply.github.com>
2025-10-07 18:01:46 -0600

    Add Calculadora interface and class stubs
    Introduced the Calculadora interface with basic arithmetic methods and a corresponding class implementing the interface. Method bodies are not yet implemented.

---
+++ Actividad_4_Calculadora/Clases/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_4_Calculadora.Clases
+{
+    public class Calculadora : Interfaces.Calculadora
+    {
+        public double Dividir(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiplicar(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Restar(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sumar(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Actividad_4_Calculadora/Interfaces/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_4_Calculadora.Interfaces
+{
+    internal interface Calculadora
+    {
+        double Sumar(double a, double b);
+        double Restar(double a, double b);
+        double Multiplicar(double a, double b);
+        double Dividir(double a, double b);
+    }
+}
